198c410b567f8c491ab558412142ec4f8afcef44
Daniel Casimir Grigg <mail@danielgrigg.com>
2013-04-21 20:57:24 +1000

    Add EXR exporting.

--- src/exru.cpp
+++ src/exru.cpp
@@ -1,22 +1,38 @@
 #include "exru.h"
 #include <vector>
+#include <string>
 #include <ImfRgbaFile.h>
 
 int write_rgba(int width, 
     int height, 
-    char* filename, 
+    const char* filename, 
     const float* raw_rgbas) {
 
-  std::vector<Imf::Rgba> pixels(width * height);
-  for (int p = 0; p < width * height; ++p) {
-    pixels[p] = Imf::Rgba(raw_rgbas[4*p+0], 
-                          raw_rgbas[4*p+1], 
-                          raw_rgbas[4*p+2],
-                          raw_rgbas[4*p+3]);
+  if (width < 0 ||
+      height < 0 ||
+      filename == NULL ||
+      std::string(filename).empty() || 
+      raw_rgbas == NULL) {
+    return 0;
   }
-  Imf::RgbaOutputFile file (filename, width, height, Imf::WRITE_RGBA);
-  file.setFrameBuffer(&pixels[0], 1, width);
-  file.writePixels (height); 
+
+  try
+  {
+    std::vector<Imf::Rgba> pixels(width * height);
+    for (int p = 0; p < width * height; ++p) {
+      pixels[p] = Imf::Rgba(raw_rgbas[4*p+0], 
+          raw_rgbas[4*p+1], 
+          raw_rgbas[4*p+2],
+          raw_rgbas[4*p+3]);
+    }
+    Imf::RgbaOutputFile file (filename, width, height, Imf::WRITE_RGBA);
+    file.setFrameBuffer(&pixels[0], 1, width);
+    file.writePixels (height); 
+  }
+  catch (...) {
+    return 0;
+  }
+
   return 1;
 }
 
--- src/exru.h
+++ src/exru.h
@@ -6,9 +6,17 @@
 extern "C" {
   
   /*
+   * Write an RGBA EXR.
+   *
+   * remarks The output image is half precision..
    * rgbas is a list of 4-float tuples.
+   *
+   * returns 1 on success
    */
-  int write_rgba(int width, int height, char* filename, const float* rgbas);
+  int write_rgba(int width, 
+      int height, 
+      const char* filename, 
+      const float* rgbas);
 }
 
 #endif
